b8490d9eb5d6a4e69420f25e7a8a9f63412c7314
sohanemon <m.sohanemon@gmail.com>
2023-01-19 21:53:42 +0600

    data fetched

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const Problem2 = () => {
+  const [contacts, setContacts] = useState([]);
 
-    return (
+  async function fetchContacts(us) {
+    let url;
+    if (us)
+      url =
+        "https://contact.mediusware.com/api/country-contacts/united%20states/";
+    else url = "https://contact.mediusware.com/api/contacts/";
 
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
-                <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
-                </div>
-                
-            </div>
+    const res = await fetch(url);
+    const data = await res.json();
+  }
+
+  return (
+    <div className='container'>
+      <div className='row justify-content-center mt-5'>
+        <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
+
+        <div className='d-flex justify-content-center gap-3'>
+          <button
+            onClick={() => fetchContacts(false)}
+            className='btn btn-lg btn-outline-primary'
+            type='button'
+          >
+            All Contacts
+          </button>
+          <button
+            onClick={() => fetchContacts(true)}
+            className='btn btn-lg btn-outline-warning'
+            type='button'
+          >
+            US Contacts
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Problem2;
